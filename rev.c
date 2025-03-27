ed657f0e79e31a3e1b9deb8394feba51e30d2aaa
AngelMariyaJose <angelmariyajose8@gmail.com>
2025-03-27 13:40:10 +0530

    Add files via upload

---
+++ rev.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fibnacci(int)
+void main(()
+{
+int num;
+printf("enter the number")
+scanf("%d",&num)
+reverse number(num);
+}
+void reverse number(int num)
+while(num!=0)
+{
+int reminder r=num%10;
+reverse=reverse*10+reminder
+num/=10;
+}
+printf("reverse of number is :"%d%n";reverse);
+}
